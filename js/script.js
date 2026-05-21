346f8310f637667f7ef1f90d5dca1a72ea5d722c
Yashin Al Fauzy <yashinfauzy@apps.ipb.ac.id>
2026-05-21 10:30:46 +0700

    llama model

--- js/script.js
+++ js/script.js
@@ -435,7 +435,7 @@
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({
-                        model: 'llama-3.3-70b-versatile',
+                        model: 'meta-llama/llama-4-scout-17b-16e-instruct',
                         messages: [{
                             role: 'user',
                             content: [
